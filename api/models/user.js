819ef73fbc4e3877494a49408c9061ee050670b5
Juan Manuel Batty Linero <jmbl1685@gmail.com>
2018-04-15 13:14:10 -0500

    JB - FIX: userModel - mongoose

--- api/models/user.js
+++ api/models/user.js
@@ -2,33 +2,17 @@
 
 const mongoose = require('../config/config')
 const Schema = mongoose.db.Schema
-const bcrypt = require('bcrypt-nodejs')
 
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   displayName: String,
   avatar: String,
-  password: { type: String, select: false },
+  password: String,
   signupDate: { type: Date, default: Date.now() },
   lastLogin: Date,
-  state: { type: Boolean, default: false},
+  state: { type: Boolean, default: false },
   providerId: String,
   //birthDate: Date
-})
-
-UserSchema.pre('save', (next) => {
-  let user = this
-  
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) return next(err)
-
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) return next(err)
-
-      user.password = hash
-      next()
-    })
-  })
 })
 
 module.exports = mongoose.db.model('User', UserSchema)
