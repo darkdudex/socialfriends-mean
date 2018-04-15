f47230b2363d6a3dd4605d6c608d3a1d58958c28
Juan Manuel Batty Linero <jmbl1685@gmail.com>
2018-04-15 13:13:22 -0500

    JB - FIX: userController.js

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,37 +2,57 @@
 
 const userModel = require('../models/user')
 const service = require('../services/services')
+const bcrypt = require('bcrypt-nodejs')
+
+function EncodePassword(password) {
+  return bcrypt.hashSync(password)
+}
+
+function DecodePassword(password, passwordEncode) {
+  return bcrypt.compareSync(password, passwordEncode);
+}
 
 async function signUp(req, res) {
   try {
-
     const user = await userModel.insertMany({
       email: req.body.email,
       displayName: req.body.displayName,
-      password: req.body.password,
+      password: EncodePassword(req.body.password),
       providerId: req.body.providerId,
       avatar: req.body.avatar
     });
-
-    return res.status(200).send({ token: service.CreateToken(user) })
-
+    return res.status(200).send({
+      token: service.CreateToken(user)
+    })
   } catch (error) {
-    return res.status(500).send({ message: `Error al crear el usuario: ${error}` })
+    return res.status(500).send({
+      message: `Error al crear el usuario: ${error}`
+    })
   }
-
 }
 
 async function signIn(req, res) {
-  userModel.find({ email: req.body.email }, (err, user) => {
-    if (err) return res.status(500).send({ message: err })
-    if (!user) return res.status(404).send({ message: 'No existe el usuario' })
-
+  try {
+    const email = req.body.email;
+    const password = req.body.password
+    const user = await userModel.findOne({ email });
     req.user = user
-    res.status(200).send({
-      message: 'Te has logueado correctamente',
-      token: service.createToken(user)
+
+    if(DecodePassword(password,user.password)){
+      return res.status(200).send({
+        token: service.CreateToken(user)
+      })
+    }
+
+    return res.status(404).send({
+      message: 'No existe el usuario' 
     })
-  })
+
+  } catch (error) {
+    return res.status(500).send({ 
+      message: 'Error en el servidor' 
+    })
+  }
 }
 
 module.exports = {
