d92ae118fd4e7b99d8c0e9b84eda6408e6ac9365
Juan Manuel Batty Linero <jmbl1685@gmail.com>
2018-04-19 21:34:50 -0500

    Update user.component.ts

--- website/src/app/components/user/user.component.ts
+++ website/src/app/components/user/user.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
