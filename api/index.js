252d6e45ca38f4d34a7348423a2224f65b37cab1
Juan Batty <jmbl1685@hotmail.com>
2018-06-18 18:08:28 -0500

    JB - Adding test structure (mocha and chai)

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const app = require('./app');
 const config = require('./config/config')
 const chalk = require('chalk')
 
-app.listen(config.port, () => {
-  console.log(`${chalk.blue('Server running')}`)
+const port = config.port;
+
+app.listen(port, () => {
+  console.log(`${chalk.blue(`Server running [http://localhost:${port}/api]`)}`)
 })
