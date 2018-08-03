59d1f3936dc267626a81c4546b2357d8849b64d5
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-08-03 15:13:26 -0500

    JB - Chat [mockup]

--- server/models/model.chat/chat.model.js
+++ server/models/model.chat/chat.model.js
@@ -3,9 +3,21 @@
 import config from '../../config/config'
 
 const { Schema } = config.mongoose
- 
-const ChatSchema = new Schema({
 
-},{ versionKey: false })
+const ChatSchema = new Schema({
+  creationDate: Date,
+  Message: String,
+  filePublication: Array,
+  userFromChat: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Required field']
+  },
+  userToChat: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Required field']
+  }
+}, { versionKey: false })
 
 export default config.mongoose.model('Chat', ChatSchema)
